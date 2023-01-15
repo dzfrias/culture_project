5fe9b00518d05271ccf5c70873164c8b237da1de
Diego <96022404+dzfrias@users.noreply.github.com>
2023-01-14 17:04:53 -0800

    feat(ui): hide top bar on scroll down

--- src/topbar.rs
+++ src/topbar.rs
@@ -1,3 +1,5 @@
+use wasm_bindgen::{prelude::Closure, JsCast, UnwrapThrowExt};
+use web_sys::HtmlElement;
 use yew::prelude::*;
 
 #[derive(Debug, PartialEq, Properties)]
@@ -9,8 +11,32 @@ pub struct Props {
 
 #[function_component(TopBar)]
 pub fn top_bar(props: &Props) -> Html {
+    let bar = use_node_ref();
+    let mut last_scroll = gloo::utils::window().scroll_y().unwrap();
+    let on_scroll = {
+        let bar_ref = bar.clone();
+        Closure::wrap(Box::new(move |_: Event| {
+            let bar = bar_ref
+                .cast::<HtmlElement>()
+                .expect("should be an html element");
+            let scroll = gloo::utils::window().scroll_y().unwrap();
+            if last_scroll < scroll {
+                bar.style()
+                    .set_property("top", &((-bar.offset_height()).to_string() + "px"))
+                    .unwrap();
+            } else {
+                bar.style().set_property("top", "0").unwrap();
+            }
+            last_scroll = scroll;
+        }) as Box<dyn FnMut(_)>)
+    };
+    gloo::utils::window()
+        .add_event_listener_with_callback("scroll", on_scroll.as_ref().unchecked_ref())
+        .unwrap_throw();
+    on_scroll.forget();
+
     html! {
-        <div id="top-bar">
+        <div id="top-bar" ref={bar}>
             <h1>{ &props.title }</h1>
             { for props.children.iter() }
         </div>
