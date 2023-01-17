8185a1b2c10334b00bbb8daa38a39ff60f1928bc
dzfrias <dzfrias@users.noreply.github.com>
2023-01-17 17:48:16 +0000

    deploy: 15ad45bbb4d66a8d34bac983f8caf31128eeb8a8

--- culture_project-e8d9c0c5cf876b0f.js
+++ culture_project-e8d9c0c5cf876b0f.js
@@ -758,7 +758,7 @@ function initSync(module) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('culture_project-a55652c63158939c_bg.wasm', import.meta.url);
+        input = new URL('culture_project-e8d9c0c5cf876b0f_bg.wasm', import.meta.url);
     }
     const imports = getImports();
 
